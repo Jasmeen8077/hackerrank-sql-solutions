17763c2ee08c7dd7bffccff21c1d6b98bb2df72b
Jasmeen Khanam <jasmeenkhanam00@gmail.com>
2025-05-25 23:04:47 +0530

    two more sql based questions

---
+++ basic_solutions/select2.sql
@@ -0,0 +1,13 @@
+/*
+
+Question : Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+
+Link : https://www.hackerrank.com/challenges/more-than-75-marks/problem?isFullScreen=true
+
+*/
+
+/*
+Answer
+*/
+
+SELECT name FROM STUDENTS WHERE marks > 75 ORDER BY RIGHT(NAME,3), ID ASC;
