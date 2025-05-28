a09c33f241783a0a4b6cb24a3c9b88ca3659b2cd
Jasmeen Khanam <jasmeenkhanam00@gmail.com>
2025-05-28 12:55:46 +0530

    dsa 3 question

---
+++ basic_solutions/dsa/dsa3.java
@@ -0,0 +1,24 @@
+/*
+ * 
+ * Question Link: https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+ * 
+ * 
+ * 
+ * Answer
+ */
+import java.util.*;
+public class Dsa3{
+    public static void Sum(int a , int b)
+    {
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String args[])
+    {
+      Scanner sc  = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b  = sc.nextInt(); 
+      Sum(a,b);
+       
+    }
+}
